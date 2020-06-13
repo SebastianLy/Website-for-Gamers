03c5598a9397d57ef073354c4b750eb633baf8e9
Sebastian Ł <sebos6_1998@o2.pl>
2020-06-13 13:50:41 +0200

    Dodałem piewrsze wyszukiwanie

--- src/Controller/SearchGameController.php
+++ src/Controller/SearchGameController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchGameController extends AbstractController
 {
-    /**
-     * @Route("/search/game", name="search_game")
-     */
+
     public function index()
     {
         return new Response(readfile("search.html"));
+    }
+
+    public function search()
+    {
+        $entity_manager = $this->getDoctrine()->getManager();
+        $title = $_POST['title'];
+        $games = $entity_manager->getRepository(Game::class)->findBy(array('title' => $title));
+        return $this->render('search_game/index.html.twig', array('games' => $games));
     }
 }
